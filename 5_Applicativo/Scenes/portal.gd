93b06c15b1005327842b46b433a116e8aec09033
DiStefanoLorenzo <96242011+DiStefanoLorenzo@users.noreply.github.com>
2024-02-09 16:45:30 +0100

    Dinamicizzazione Portali + Correzione Viewport

---
+++ 5_Applicativo/Scenes/portal.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+# PORTAL
+
+@export var viewport = false
+@export var exit_position = false
+@export var destination_portal = false
+@export var checkpoint_enabled = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
