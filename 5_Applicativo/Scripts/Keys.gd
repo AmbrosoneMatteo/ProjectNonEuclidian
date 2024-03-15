1d8c02e4be8c8cb7c127e4512f7c0fd153211ee2
AmbrosoneMatteo <matteo.ambrosone@samtrevano.ch>
2024-03-15 15:42:24 +0100

    Commit of 15.03.2024 -> Keymap options implementation

--- 5_Applicativo/Scripts/Keys.gd
+++ 5_Applicativo/Scripts/Keys.gd
@@ -1,7 +1,7 @@
 extends Control
 
 
-var input_selected: String
+var input_selected: String = "null"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,11 +11,11 @@ func _ready():
 func _process(delta):
 	pass
 
-func _input(event):
-	if event is InputEventKey:
-		InputMap.erase_action(input_selected)
-		InputMap.add_action(input_selected)
-		InputMap.action_add_event(input_selected, event.keycode)
+#func _input(event):
+#	if event is InputEventKey and input_selected!="null":
+#		InputMap.erase_action(input_selected)
+#		InputMap.add_action(input_selected)
+#		InputMap.action_add_event(input_selected, event.keycode)
 			
 func _on_key_pressed(input: String):
 	input_selected = input
